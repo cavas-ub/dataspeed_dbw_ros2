55b1e3bfe914e8b12855bf354fac7a240bf12af6
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2023-01-17 16:56:47 -0500

    Update install scripts for ROS2

--- dbw_fca/scripts/ros_install.bash
+++ dbw_fca/scripts/ros_install.bash
@@ -22,6 +22,7 @@ sudo apt-get purge unity-webapps-common apport -y
 
 # Upgrade
 echo "Upgrading system..."
+sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get update
 sudo apt-get dist-upgrade -y
@@ -29,14 +30,10 @@ sudo apt-get autoremove -y
 
 # Determine ROS version to install
 codename=`lsb_release -sc`
-if   [ "$codename" = "trusty" ]; then
-  ROS_DISTRO=indigo
-elif [ "$codename" = "xenial" ]; then
-  ROS_DISTRO=kinetic
-elif [ "$codename" = "bionic" ]; then
-  ROS_DISTRO=melodic
-elif [ "$codename" = "focal" ]; then
-  ROS_DISTRO=noetic
+if   [ "$codename" = "focal" ]; then
+  ROS_DISTRO=foxy
+elif [ "$codename" = "jammy" ]; then
+  ROS_DISTRO=humble
 else
   echo "Unable to determine ROS version for OS codename '"$codename"'"
   exit 1
@@ -44,11 +41,11 @@ fi
 
 # Install ROS
 echo "Installing ROS $ROS_DISTRO..."
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-get install -y curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y ros-$ROS_DISTRO-desktop
+sudo apt-get install -y ros-$ROS_DISTRO-desktop python3-rosdep
 sudo rosdep init
 
 # Update rosdep rules
@@ -57,14 +54,16 @@ rosdep update
 
 # Setup environment
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+echo "export RCUTILS_COLORIZED_OUTPUT=1" >> ~/.bashrc
+echo "export ROS_LOCALHOST_ONLY=1" >> ~/.bashrc
 
 # Install SDK
 echo "Installing SDK..."
-bash <(wget -q -O - https://bitbucket.org/DataspeedInc/dbw_fca_ros/raw/master/dbw_fca/scripts/sdk_install.bash)
+bash <(wget -q -O - https://bitbucket.org/DataspeedInc/dbw_ros/raw/ros2/dbw_fca/scripts/sdk_install.bash)
 
 # Configure startup script
 mkdir -p $HOME/.config/autostart
-wget -q https://bitbucket.org/DataspeedInc/dbw_fca_ros/raw/master/dbw_fca/scripts/joystick_demo.desktop -O $HOME/.config/autostart/joystick_demo.desktop
+wget -q https://bitbucket.org/DataspeedInc/dbw_ros/raw/ros2/dbw_fca/scripts/joystick_demo.desktop -O $HOME/.config/autostart/joystick_demo.desktop
 
 ### Misc fixes ###
 # Fix launcher icons
--- dbw_fca/scripts/sdk_install.bash
+++ dbw_fca/scripts/sdk_install.bash
@@ -1,18 +1,12 @@
 #! /bin/bash
 
 # Detect ROS version
-if [ -e /opt/ros/indigo/setup.bash ]; then
-  echo "Detected ROS Indigo."
-  source /opt/ros/indigo/setup.bash
-elif [ -e /opt/ros/kinetic/setup.bash ]; then
-  echo "Detected ROS Kinetic."
-  source /opt/ros/kinetic/setup.bash
-elif [ -e /opt/ros/melodic/setup.bash ]; then
-  echo "Detected ROS Melodic."
-  source /opt/ros/melodic/setup.bash
-elif [ -e /opt/ros/noetic/setup.bash ]; then
-  echo "Detected ROS Noetic."
-  source /opt/ros/noetic/setup.bash
+if [ -e /opt/ros/foxy/setup.bash ]; then
+  echo "Detected ROS Foxy."
+  source /opt/ros/foxy/setup.bash
+elif [ -e /opt/ros/humble/setup.bash ]; then
+  echo "Detected ROS Humble."
+  source /opt/ros/humble/setup.bash
 else
   echo "Failed to detected ROS version."
   exit 1
@@ -20,8 +14,9 @@ fi
 
 # Setup apt-get
 echo "Adding Dataspeed server to apt..."
-sudo sh -c 'echo "deb [ arch=amd64 ] http://packages.dataspeedinc.com/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-dataspeed-public.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 66F84AE1EB71A8AC108087DCAF677210FF6D3CDA
+sudo apt-get install -y curl
+sudo curl -sSL https://bitbucket.org/DataspeedInc/ros_binaries/raw/master/dataspeed.key -o /usr/share/keyrings/dataspeed-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/dataspeed-archive-keyring.gpg] http://packages.dataspeedinc.com/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2-dataspeed-public.list > /dev/null
 sudo apt-get update
 
 # Setup rosdep
@@ -29,7 +24,7 @@ echo "Setting up rosdep..."
 if [ -z "$ROS_DISTRO" ]; then
   echo "Error! ROS not detected. Not updating rosdep!"
 else
-  sudo sh -c 'echo "yaml http://packages.dataspeedinc.com/ros/ros-public-'$ROS_DISTRO'.yaml '$ROS_DISTRO'" > /etc/ros/rosdep/sources.list.d/30-dataspeed-public-'$ROS_DISTRO'.list'
+  sudo sh -c 'echo "yaml http://packages.dataspeedinc.com/ros2/ros-public-'$ROS_DISTRO'.yaml '$ROS_DISTRO'" > /etc/ros/rosdep/sources.list.d/30-dataspeed-public-'$ROS_DISTRO'.list'
   rosdep update
 fi
 
